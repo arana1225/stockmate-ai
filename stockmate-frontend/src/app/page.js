82a3220bcc7de864a3d99b7045b7dd27774d8148
Jigyasa Raghav <jigyasaraghav@Jigyasas-MacBook-Air.local>
2025-04-28 19:29:41 +0530

    Added again stockmate-frontend folder

---
+++ stockmate-frontend/src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <main style={{ padding: "2rem" }}>
+        <h1>Welcome to StocksMate🚀</h1>
+        <p>Your personal AI-powered stock trading assistant!</p>
+      </main>
+    )
+  }
+  
